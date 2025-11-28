e3ba2588d4ef927948b328ab046c6ec0848c7916
Epril Tiew <epriltiew@gmail.com>
2025-11-28 19:13:30 +0800

    delete auto billid in sql

--- create-db-template.sql
+++ create-db-template.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS order_management;
 USE order_management;
 
 CREATE TABLE billing (
-    bill_id INT PRIMARY KEY AUTO_INCREMENT,
+    bill_id INT PRIMARY KEY,
     order_id INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     total_amt DECIMAL(10, 2) NOT NULL,
