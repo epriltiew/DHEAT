e78f2cd61204f0ec75385b9a5a3f67dd3a7a49d0
Epril Tiew <epriltiew@gmail.com>
2025-11-28 18:55:00 +0800

    Change variable name

--- create-db-template.sql
+++ create-db-template.sql
@@ -5,7 +5,7 @@ CREATE TABLE billing (
     bill_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    total_amount DECIMAL(10, 2) NOT NULL,
+    total_amt DECIMAL(10, 2) NOT NULL,
     payment_method VARCHAR(50) NOT NULL,
     billing_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (order_id) REFERENCES orderlist(order_id)
