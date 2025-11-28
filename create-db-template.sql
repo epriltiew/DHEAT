79e2c9b7de906b166b059c501413eca828250ecc
Epril Tiew <epriltiew@gmail.com>
2025-11-28 20:01:01 +0800

    Update billing database

--- create-db-template.sql
+++ create-db-template.sql
@@ -2,12 +2,14 @@ CREATE DATABASE IF NOT EXISTS order_management;
 USE order_management;
 
 CREATE TABLE billing (
-    bill_id INT PRIMARY KEY,
-    order_id INT NOT NULL,
-    price DECIMAL(10, 2) NOT NULL,
-    total_amt DECIMAL(10, 2) NOT NULL,
-    payment_method VARCHAR(50) NOT NULL,
-    billing_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (order_id) REFERENCES orderlist(order_id)
+    bill_id VARCHAR(10) PRIMARY KEY,
+    order_id VARCAHR(10) NOT NULL,
+    price DOUBLE NOT NULL,
+    total_amt DOUBLE NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    billing_date DATETIME NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES cus_order(order_id)
 );
+
+SELECT * FROM billing;
     
