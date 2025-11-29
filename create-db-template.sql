6b683d00e3e57c0d8b6e2e8679d01242be7cceab
Epril Tiew <epriltiew@gmail.com>
2025-11-30 00:43:51 +0800

    Complete billing and sql database

--- create-db-template.sql
+++ create-db-template.sql
@@ -1,15 +1,14 @@
-CREATE DATABASE IF NOT EXISTS order_management;
-USE order_management;
+CREATE DATABASE IF NOT EXISTS billing;
+USE billing;
 
-CREATE TABLE billing (
+CREATE TABLE bills (
     bill_id VARCHAR(10) PRIMARY KEY,
     order_id VARCAHR(10) NOT NULL,
-    price DOUBLE NOT NULL,
-    total_amt DOUBLE NOT NULL,
+    grand_total DECIMAL(10, 2) NOT NULL,
     payment_method VARCHAR(20) NOT NULL,
-    billing_date DATETIME NOT NULL,
+    billing_date DATE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES cus_order(order_id)
 );
 
-SELECT * FROM billing;
+SELECT * FROM bills;
     
